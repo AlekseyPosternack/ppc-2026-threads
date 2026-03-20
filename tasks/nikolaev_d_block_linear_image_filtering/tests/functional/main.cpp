0ce0239fe4551cd8d11ed43522accd0f525785ce
Uzema <uzema_2k16@mail.ru>
2026-03-21 01:40:32 +0300

    template

--- tasks/nikolaev_d_block_linear_image_filtering/tests/functional/main.cpp
+++ tasks/nikolaev_d_block_linear_image_filtering/tests/functional/main.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 
 #include "nikolaev_d_block_linear_image_filtering/common/include/common.hpp"
+#include "nikolaev_d_block_linear_image_filtering/omp/include/ops_omp.hpp"
 #include "nikolaev_d_block_linear_image_filtering/seq/include/ops_seq.hpp"
 #include "util/include/func_test_util.hpp"
 #include "util/include/util.hpp"
@@ -61,8 +62,10 @@ const std::array<TestType, 5> kTestParam = {
     std::make_tuple(std::make_tuple(4, 1, std::vector<uint8_t>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}),
                     std::vector<uint8_t>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, "4x1")};
 
-const auto kTestTasksList = ppc::util::AddFuncTask<NikolaevDBlockLinearImageFilteringSEQ, InType>(
-    kTestParam, PPC_SETTINGS_nikolaev_d_block_linear_image_filtering);
+const auto kTestTasksList = std::tuple_cat(ppc::util::AddFuncTask<NikolaevDBlockLinearImageFilteringSEQ, InType>(
+                                               kTestParam, PPC_SETTINGS_nikolaev_d_block_linear_image_filtering),
+                                           ppc::util::AddFuncTask<NikolaevDBlockLinearImageFilteringOMP, InType>(
+                                               kTestParam, PPC_SETTINGS_nikolaev_d_block_linear_image_filtering));
 
 const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 
